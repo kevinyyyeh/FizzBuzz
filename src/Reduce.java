bce6dd6319e15bd3c6911cc792d8709dc74c65cd
kevinyeh <kevin.yeh@mail.utoronto.ca>
2024-09-09 18:58:08 -0400

    refactored to extract helper method

---
+++ src/Reduce.java
@@ -0,0 +1,26 @@
+public class Reduce {
+    public static void main(String[] args) {
+
+        int n = 100;
+        int count = 0;
+
+        while (n > 0) {
+
+            boolean even = n % 2 == 0;
+            boolean odd = n % 2 == 1;
+
+            if (even) {
+
+                n = n / 2;
+
+            } else {
+
+                n = n - 1;
+            }
+            count++;
+        }
+
+        System.out.println(count);
+
+    }
+}
